91f8b2dc436584c4b76755086c4c5006d873a31e
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2011-11-20 09:45:23 +0000

    Move fourChars() function a bit more sensibly

--- static/js/rpmap.js
+++ static/js/rpmap.js
@@ -1,10 +1,10 @@
 $(function() {
     var PseudoGuid = new (function() {
+        var fourChars = function() {
+                return (((1 + Math.random()) * 0x10000)|0).toString(16).substring(1).toUpperCase();
+        };
         this.empty = "00000000-0000-0000-0000-000000000000";
         this.GetNew = function() {
-            var fourChars = function() {
-                    return (((1 + Math.random()) * 0x10000)|0).toString(16).substring(1).toUpperCase();
-            }
             return (fourChars() + fourChars() + "-" + fourChars() + "-" + fourChars() + "-" + fourChars() + "-" + fourChars() + fourChars() + fourChars());
         };
     })();
